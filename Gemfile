4e08289be8148f70085e9b93872a4071eae0f6bb
tonnybrito <tonny@devlandia.net>
2018-11-20 18:15:52 +0000

    include model ability

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'rails_admin'
 gem 'devise'
+gem 'cancancan'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181119180538) do
+ActiveRecord::Schema.define(version: 20181120162844) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "country"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20181119180538) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sale_id"
     t.index ["product_id"], name: "index_product_quantities_on_product_id"
     t.index ["user_id"], name: "index_product_quantities_on_user_id"
   end
@@ -78,6 +79,7 @@ ActiveRecord::Schema.define(version: 20181119180538) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price"
   end
 
   create_table "sales", force: :cascade do |t|
@@ -106,6 +108,15 @@ ActiveRecord::Schema.define(version: 20181119180538) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
